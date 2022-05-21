90c076cd516a2d912cd282dba8fded67339a96b8
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-22 01:25:40 +0600

    Added a logo in navbar and cleared some warnings

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import logo from '../../Assets/logo.png';
 
 const Navbar = ({ children }) => {
     return (
 
-        <div class="drawer">
-            <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col">
-                <div class="w-full navbar bg-base-100 lg:px-24 shadow-2xl">
-                    <div class="flex-none lg:hidden">
-                        <label for="my-drawer-3" class="btn btn-square btn-ghost">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        <div className="drawer">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+                <div className="w-full navbar bg-base-100 lg:px-24 shadow-2xl">
+                    <div className="flex-none lg:hidden">
+                        <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                         </label>
                     </div>
-                    <div class="flex-1 px-2 mx-2"><Link to="/" class="btn btn-ghost normal-case text-2xl">Credible Technologies Ltd.</Link></div>
-                    <div class="flex-none hidden lg:block">
-                        <ul class="menu menu-horizontal">
+                    <div className="flex-1 px-2 mx-2"><Link to="/" className="btn btn-ghost normal-case text-2xl"><img className='w-9 mr-2' src={logo} alt="" /> Credible Technologies Ltd.</Link></div>
+                    <div className="flex-none hidden lg:block">
+                        <ul className="menu menu-horizontal">
                             <li className='mx-1'><NavLink className="rounded-3xl" to="/home">HOME</NavLink></li>
                             <li className='mx-1'><NavLink className="rounded-3xl" to="/products">PRODUCTS</NavLink></li>
                         </ul>
                     </div>
 
-                    <Link to="/login" class="btn btn-neutral ml-3">LOG IN</Link>
+                    <Link to="/login" className="btn btn-neutral ml-3">LOG IN</Link>
 
                 </div>
 
@@ -29,9 +30,9 @@ const Navbar = ({ children }) => {
                 {children}
 
             </div>
-            <div class="drawer-side">
-                <label for="my-drawer-3" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+                <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
                     <li className='my-2'><NavLink to="/">Sidebar Item 1</NavLink></li>
                     <li className='my-2'><NavLink to="/">Sidebar Item 2</NavLink></li>
 

7774160ddd4f452bde6b38866df486584bc2e103
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-22 15:54:56 +0600

    Added some animation in login, signup, reset password route

--- src/Components/ResetPass/ResetPass.js
+++ src/Components/ResetPass/ResetPass.js
@@ -7,6 +7,7 @@ import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
 import auth from '../../Firebase/firebase.init';
 import { toast } from 'react-toastify';
 import Loading from '../Loading/Loading';
+import { Slide } from 'react-reveal';
 
 const ResetPass = () => {
 
@@ -32,46 +33,50 @@ const ResetPass = () => {
 
                     :
 
-                    <div className="hero min-h-screen bg-gradient-to-tr from-primary to-secondary">
+                    <div className="hero min-h-screen bg-gradient-to-tr from-primary to-secondary overflow-x-hidden">
                         <div className="hero-content">
-                            <div className='lg:w-3/6 lg:flex lg:justify-end'>
-                                <div className="card flex-shrink-0 lg:w-96 w-80 shadow-2xl bg-base-100">
-                                    <h1 className='text-center text-2xl font-bold text-primary mt-9'><img className='w-8 inline-block mt-[-6px]' src={logo} alt="" /> Credible Technologies</h1>
-                                    <p className='text-center font-bold mt-3'>RESET PASSWORD</p>
-                                    <div className="card-body">
+                            <Slide left>
+                                <div className='lg:w-3/6 lg:flex lg:justify-end'>
+                                    <div className="card flex-shrink-0 lg:w-96 w-80 shadow-2xl bg-base-100">
+                                        <h1 className='text-center text-2xl font-bold text-primary mt-9'><img className='w-8 inline-block mt-[-6px]' src={logo} alt="" /> Credible Technologies</h1>
+                                        <p className='text-center font-bold mt-3'>FORGOT PASSWORD?</p>
+                                        <div className="card-body">
 
-                                        <form onSubmit={handleSubmit(onSubmit)}>
-                                            <div className="form-control">
-                                                <label className="label">
-                                                    <span className="label-text">Your existing email</span>
-                                                </label>
-                                                <input type="email" placeholder="email" className="input input-bordered" {...register("email", {
-                                                    required: {
-                                                        value: true,
-                                                        message: "Email is required"
-                                                    },
-                                                    pattern: {
-                                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                                                        message: "Provide a valid email"
-                                                    }
-                                                })} />
-                                                <label className="label">
-                                                    {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                                                    {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                                                </label>
-                                            </div>
+                                            <form onSubmit={handleSubmit(onSubmit)}>
+                                                <div className="form-control">
+                                                    <label className="label">
+                                                        <span className="label-text">Your existing email</span>
+                                                    </label>
+                                                    <input type="email" placeholder="email" className="input input-bordered" {...register("email", {
+                                                        required: {
+                                                            value: true,
+                                                            message: "Email is required"
+                                                        },
+                                                        pattern: {
+                                                            value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                                            message: "Provide a valid email"
+                                                        }
+                                                    })} />
+                                                    <label className="label">
+                                                        {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                                        {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                                    </label>
+                                                </div>
 
-                                            <input type="submit" value='Reset Password' className="btn text-white w-full mt-3" />
-                                        </form>
+                                                <input type="submit" value='Reset Password' className="btn text-white w-full mt-3" />
+                                            </form>
 
 
-                                        <Link to="/Login" className="text-center text-sm label-text-alt link link-hover mt-2">Return to login page</Link>
+                                            <Link to="/Login" className="text-center text-sm label-text-alt link link-hover mt-2">Return to login page</Link>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="hidden lg:block w-8/12">
-                                <img className='mx-auto w-4/5' src={resetPass} alt="" />
-                            </div>
+                            </Slide>
+                            <Slide right>
+                                <div className="hidden lg:block w-8/12">
+                                    <img className='mx-auto w-4/5' src={resetPass} alt="" />
+                                </div>
+                            </Slide>
                         </div>
                     </div>
             }
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -9,6 +9,7 @@ import auth from '../../Firebase/firebase.init';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { toast } from 'react-toastify';
 import Loading from '../Loading/Loading';
+import { Slide } from 'react-reveal';
 
 const SignUp = () => {
 
@@ -37,84 +38,88 @@ const SignUp = () => {
 
                     :
 
-                    <div className="hero min-h-screen bg-gradient-to-tr from-primary to-secondary">
+                    <div className="hero min-h-screen bg-gradient-to-tr from-primary to-secondary overflow-x-hidden">
                         <div className="hero-content">
-                            <div className='lg:w-3/6 lg:flex lg:justify-end'>
-                                <div className="card flex-shrink-0 lg:w-96 w-80 shadow-2xl bg-base-100">
-                                    <h1 className='text-center text-2xl font-bold text-primary mt-9'><img className='w-8 inline-block mt-[-6px]' src={logo} alt="" /> Credible Technologies</h1>
-                                    <p className='text-center font-bold mt-3'>CREATE ACCOUNT</p>
-                                    <div className="card-body">
-                                        <form onSubmit={handleSubmit(onSubmit)}>
-                                            <div className="form-control">
-                                                <label className="label">
-                                                    <span className="label-text">Name</span>
-                                                </label>
-                                                <input type="text" placeholder="your name" className="input input-bordered" {...register("name", {
-                                                    required: {
-                                                        value: true,
-                                                        message: "Name is required"
-                                                    }
-                                                })} />
-                                                <label className="label">
-                                                    {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
-                                                </label>
-                                            </div>
+                            <Slide left>
+                                <div className='lg:w-3/6 lg:flex lg:justify-end'>
+                                    <div className="card flex-shrink-0 lg:w-96 w-80 shadow-2xl bg-base-100">
+                                        <h1 className='text-center text-2xl font-bold text-primary mt-9'><img className='w-8 inline-block mt-[-6px]' src={logo} alt="" /> Credible Technologies</h1>
+                                        <p className='text-center font-bold mt-3'>CREATE ACCOUNT</p>
+                                        <div className="card-body">
+                                            <form onSubmit={handleSubmit(onSubmit)}>
+                                                <div className="form-control">
+                                                    <label className="label">
+                                                        <span className="label-text">Name</span>
+                                                    </label>
+                                                    <input type="text" placeholder="your name" className="input input-bordered" {...register("name", {
+                                                        required: {
+                                                            value: true,
+                                                            message: "Name is required"
+                                                        }
+                                                    })} />
+                                                    <label className="label">
+                                                        {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                                                    </label>
+                                                </div>
 
-                                            <div className="form-control">
-                                                <label className="label">
-                                                    <span className="label-text">Email</span>
-                                                </label>
-                                                <input type="email" placeholder="email" className="input input-bordered" {...register("email", {
-                                                    required: {
-                                                        value: true,
-                                                        message: "Email is required"
-                                                    },
-                                                    pattern: {
-                                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                                                        message: "Provide a valid email"
-                                                    }
-                                                })} />
-                                                <label className="label">
-                                                    {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                                                    {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                                                </label>
-                                            </div>
+                                                <div className="form-control">
+                                                    <label className="label">
+                                                        <span className="label-text">Email</span>
+                                                    </label>
+                                                    <input type="email" placeholder="email" className="input input-bordered" {...register("email", {
+                                                        required: {
+                                                            value: true,
+                                                            message: "Email is required"
+                                                        },
+                                                        pattern: {
+                                                            value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                                            message: "Provide a valid email"
+                                                        }
+                                                    })} />
+                                                    <label className="label">
+                                                        {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                                        {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                                    </label>
+                                                </div>
 
-                                            <div className="form-control">
-                                                <label className="label">
-                                                    <span className="label-text">Password</span>
-                                                </label>
-                                                <input type="password" placeholder="password" className="input input-bordered" {...register("password", {
-                                                    required: {
-                                                        value: true,
-                                                        message: "Password is required"
-                                                    },
-                                                    minLength: {
-                                                        value: 6,
-                                                        message: "Minimum 6 characters required"
-                                                    }
-                                                })} />
-                                                <label className="label">
-                                                    {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
-                                                    {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
-                                                </label>
-                                            </div>
+                                                <div className="form-control">
+                                                    <label className="label">
+                                                        <span className="label-text">Password</span>
+                                                    </label>
+                                                    <input type="password" placeholder="password" className="input input-bordered" {...register("password", {
+                                                        required: {
+                                                            value: true,
+                                                            message: "Password is required"
+                                                        },
+                                                        minLength: {
+                                                            value: 6,
+                                                            message: "Minimum 6 characters required"
+                                                        }
+                                                    })} />
+                                                    <label className="label">
+                                                        {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                                        {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                                    </label>
+                                                </div>
 
-                                            <input type="submit" value='Sign Up' className="btn text-white w-full mt-3" />
-                                        </form>
-                                        <div className="divider">OR</div>
-                                        <div className="form-control">
-                                            <button onClick={() => signInWithGoogle()} className="btn btn-outline" >
-                                                <FcGoogle className='text-2xl mr-2' /> Continue with google
-                                            </button>
+                                                <input type="submit" value='Sign Up' className="btn text-white w-full mt-3" />
+                                            </form>
+                                            <div className="divider">OR</div>
+                                            <div className="form-control">
+                                                <button onClick={() => signInWithGoogle()} className="btn btn-outline" >
+                                                    <FcGoogle className='text-2xl mr-2' /> Continue with google
+                                                </button>
+                                            </div>
+                                            <Link to="/login" className="text-center text-sm label-text-alt link link-hover mt-2">Already have an account? Login</Link>
                                         </div>
-                                        <Link to="/login" className="text-center text-sm label-text-alt link link-hover mt-2">Already have an account? Login</Link>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="hidden lg:block w-full ml-14">
-                                <img className='mx-auto w-full' src={registration} alt="" />
-                            </div>
+                            </Slide>
+                            <Slide right>
+                                <div className="hidden lg:block w-full ml-14">
+                                    <img className='mx-auto w-full' src={registration} alt="" />
+                                </div>
+                            </Slide>
                         </div>
                     </div>
             }

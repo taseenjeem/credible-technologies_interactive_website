eecd8ddb4f9619dbbec94e2a00cfaecb1d5bbf9a
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-22 22:59:55 +0600

    Added invalid page

--- src/Components/InvalidPage/InvalidPage.js
+++ src/Components/InvalidPage/InvalidPage.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import errorImg from '../../Assets/404.png';
+import { FaArrowLeft } from 'react-icons/fa';
 
 const InvalidPage = () => {
     return (
-        <div>
-
-        </div>
+        <section>
+            <div className='h-screen'>
+                <img className='lg:w-2/5 mx-auto mt-16' src={errorImg} alt="" />
+                <Link to="/" className='flex justify-center'>
+                    <button className='btn'><FaArrowLeft className='mr-2' />Go back to Home</button>
+                </Link>
+            </div>
+        </section>
     );
 };
 

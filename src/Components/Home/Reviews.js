c0d515f0e3ad3c1af3c090dc1d5d49b47ec7710b
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-23 01:40:59 +0600

    Customer review updated

--- src/Components/Home/Reviews.js
+++ src/Components/Home/Reviews.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import quote from '../../Assets/Icons/quote.png';
+import Review from './Review';
 
 const Reviews = () => {
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch("reviews.json")
+            .then(res => res.json())
+            .then(data => setReviews(data));
+    }, [])
+
     return (
-        <section className='lg:px-24 px-3'>
+        <section className='lg:px-24 px-3 mb-32'>
             <div className='flex justify-between items-center'>
                 <div>
                     <h3 className='text-primary text-xl font-bold'>Reviews</h3>
@@ -11,20 +21,11 @@ const Reviews = () => {
                 </div>
                 <img className='lg:w-48 w-24' src={quote} alt="" />
             </div>
-            <div>
+            <div className='grid lg:grid-cols-3 gap-7'>
 
-                <div class="max-w-md py-4 px-8 bg-white shadow-xl border rounded-lg my-20">
-                    <div class="flex justify-center md:justify-end -mt-16">
-                        <img class="w-20 h-20 object-cover rounded-full border-2 border-primary" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="" />
-                    </div>
-                    <div>
-                        <h2 class="text-gray-800 text-3xl font-semibold">John Doe</h2>
-                        <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
-                    </div>
-                    <div class="flex justify-end mt-4">
-                        <a href="/" class="text-md text-gray-400 font-medium ">USA</a>
-                    </div>
-                </div>
+                {
+                    reviews.map(r => <Review key={r.id} review={r} />)
+                }
 
             </div>
         </section>

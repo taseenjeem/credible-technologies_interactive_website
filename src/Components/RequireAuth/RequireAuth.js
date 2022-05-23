f7e768ed6ea986308c26e8ce63d84e6aa1fe6de6
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-23 18:09:22 +0600

    Added require auth route

---
+++ src/Components/RequireAuth/RequireAuth.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../Firebase/firebase.init';
+import Loading from '../Loading/Loading';
+
+
+const RequireAuth = ({ children }) => {
+
+    const [user, loading] = useAuthState(auth);
+    const location = useLocation();
+
+    if (loading) {
+        return <Loading />
+    }
+
+    if (!user) {
+        toast.warning("You need to login first");
+    }
+
+    if (user && user.photoURL) {
+        return children;
+    }
+    else {
+        return <Navigate to='/login' state={{ from: location }} replace></Navigate>
+    }
+};
+
+export default RequireAuth;

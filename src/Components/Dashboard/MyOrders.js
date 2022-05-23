63d3acb1695ec44c7c331d2d201b0e82770b5646
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-24 03:24:36 +0600

    Showed my orders

--- src/Components/Dashboard/MyOrders.js
+++ src/Components/Dashboard/MyOrders.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase/firebase.init';
 
 const MyOrders = () => {
+
+    const [user] = useAuthState(auth);
+
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/my-orders?customerEmail=${user?.email}`)
+            .then(res => res.json())
+            .then(data => setOrders(data))
+    }, [user])
+
     return (
         <div>
-            orders
+            <h1 className='text-center text-2xl font-semibold my-4'>Your Orders : {orders.length}</h1>
         </div>
     );
 };

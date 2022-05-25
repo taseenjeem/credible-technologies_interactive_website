8082f5ab1d7ef7baad8962f06fe0a1c91f81d58d
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-26 01:06:15 +0600

    Added the my portfolio

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import PageTitle from '../PageTitle/PageTitle';
 
 const Blogs = () => {
     return (
         <div className='lg:px-28 lg:p-3'>
+
+            <PageTitle title="Blogs" />
+
             <div className='p-10 rounded-3xl bg-white shadow-2xl my-14'>
                 <h1 className='text-3xl font-semibold'>How will you improve the performance of a React Application?</h1>
                 <p className='mt-3'>
--- src/Components/MyPorfolio/MyPortfolio.js
+++ src/Components/MyPorfolio/MyPortfolio.js
@@ -1,10 +1,168 @@
 import React from 'react';
+import photo from '../../Assets/photo.png';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+import html from '../../Assets/Icons/icons8-html-5-240.png';
+import css from '../../Assets/Icons/icons8-css3-240.png';
+import js from '../../Assets/Icons/icons8-javascript-240.png';
+import bootstrap from '../../Assets/Icons/icons8-bootstrap-240.png';
+import tailwind from '../../Assets/Icons/tailwind-css.png';
+import react from '../../Assets/Icons/icons8-react-native-240.png';
+import node from '../../Assets/Icons/icons8-node-js-144.png';
+import mongo from '../../Assets/Icons/icons8-mongodb-240.png';
+import express from '../../Assets/Icons/Expressjs.png';
+import project1 from '../../Assets/project (1).png';
+import project2 from '../../Assets/project (2).png';
+import project3 from '../../Assets/project (3).png';
 
 const MyPortfolio = () => {
     return (
-        <div>
 
-        </div>
+
+
+        <main class="profile-page">
+            <section class="relative block h-500-px">
+                <div class="absolute top-0 w-full h-full bg-center bg-cover" style={{ backgroundImage: "url(https://cdn.pixabay.com/photo/2018/11/29/14/01/mountains-3845777_960_720.jpg)" }}>
+                    <span id="blackOverlay" class="w-full h-full absolute opacity-50 bg-black"></span>
+                </div>
+                <div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px" >
+                    <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+                        <polygon class="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
+                    </svg>
+                </div>
+            </section>
+            <section class="relative py-16 bg-blueGray-200">
+                <div class="container mx-auto px-4 lg:px-28">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-3xl -mt-64">
+                        <div class="px-6">
+                            <div class="flex flex-wrap justify-center">
+                                <div class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
+                                    <div class="relative">
+                                        <img alt="..." src={photo} class="shadow-2xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px" />
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="text-center mt-28">
+                                <h3 class="text-4xl font-semibold leading-normal  text-blueGray-700 mb-2">
+                                    Md. Ta-Seen Fuad Jeem
+                                </h3>
+                                <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                                    <FaMapMarkerAlt className='text-blueGray-400 -mt-1 mr-2 text-xl inline-block' />
+                                    Dhaka, Bangladesh
+                                </div>
+                                <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
+                                    <MdEmail className='text-blueGray-400 -mt-1 mr-2 text-xl inline-block' />
+                                    mdtaseenfuad@gmail.com
+                                </div>
+                                <div class="mb-2 text-blueGray-600 mt-10">
+                                    <i class="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
+                                    Junior Web-Developer
+                                </div>
+                                <div class="mb-2 text-blueGray-600">
+                                    <i class="fas fa-university mr-2 text-lg text-blueGray-400"></i>
+                                    BSC in Computer Science Engineering, Tejgaon Collage, Dhaka
+                                </div>
+                            </div>
+                            <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
+                                <div class="flex flex-wrap justify-center">
+                                    <div class="w-full lg:w-9/12 px-4">
+
+                                        <h1 className='text-3xl font-semibold underline underline-offset-8'>Skills Or Technologies</h1>
+
+                                        <div className='grid grid-cols-2 gap-7 mt-10'>
+
+                                            <div className='flex justify-center items-center'>
+                                                <img src={html} className="w-20" alt="" />
+                                                <h2 className='text-xl font-semibold'>HTML 5</h2>
+                                            </div>
+                                            <div className='flex justify-center items-center'>
+                                                <img src={css} className="w-20" alt="" />
+                                                <h2 className='text-xl font-semibold'>CSS 3</h2>
+                                            </div>
+                                            <div className='flex justify-center items-center'>
+                                                <img src={bootstrap} className="w-20" alt="" />
+                                                <h2 className='text-xl font-semibold'>Bootstrap</h2>
+                                            </div>
+                                            <div className='flex justify-center items-center'>
+                                                <img src={tailwind} className="w-20" alt="" />
+                                                <h2 className='text-xl font-semibold'>Tailwind CSS</h2>
+                                            </div>
+                                            <div className='flex justify-center items-center'>
+                                                <img src={js} className="w-20" alt="" />
+                                                <h2 className='text-xl font-semibold'>JavaScript</h2>
+                                            </div>
+                                            <div className='flex justify-center items-center'>
+                                                <img src={react} className="w-20" alt="" />
+                                                <h2 className='text-xl font-semibold'>React JS</h2>
+                                            </div>
+                                            <div className='flex justify-center items-center'>
+                                                <img src={mongo} className="w-20" alt="" />
+                                                <h2 className='text-xl font-semibold'>MongoDB</h2>
+                                            </div>
+                                            <div className='flex justify-center items-center'>
+                                                <img src={node} className="w-20" alt="" />
+                                                <h2 className='text-xl font-semibold'>Node JS</h2>
+                                            </div>
+
+                                        </div>
+
+                                        <div className='mt-7'>
+                                            <img src={express} className="w-40 mx-auto" alt="" />
+                                            <h2 className='text-xl font-semibold'>Express JS</h2>
+                                        </div>
+
+                                        <div className='mt-20'>
+
+                                            <h1 className='text-3xl font-semibold'>My own recent project</h1>
+
+                                            <div className="grid lg:grid-cols-3 gap-6 mt-16">
+
+                                                <div class="card bg-white shadow-2xl">
+                                                    <figure><img src={project1} alt="Project" /></figure>
+                                                    <div class="card-body">
+                                                        <h2 class="text-center text-xl font-bold">Steve's Lenses</h2>
+                                                        <p className='my-3'>An independent service provider</p>
+                                                        <div class="card-actions justify-end">
+                                                            <a href="https://photographer-portfolio-8cbb0.web.app/"><button class="btn btn-primary">Visit</button></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="card bg-white shadow-2xl">
+                                                    <figure><img src={project2} alt="Project" /></figure>
+                                                    <div class="card-body">
+                                                        <h2 class="text-center text-xl font-bold">SONY Bangladesh</h2>
+                                                        <p className='my-3'>UI, React route based website</p>
+                                                        <div class="card-actions justify-end">
+                                                            <a href="https://sony-bangladesh.netlify.app/"><button class="btn btn-primary">Visit</button></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="card bg-white shadow-2xl">
+                                                    <figure><img src={project3} alt="Project" /></figure>
+                                                    <div class="card-body">
+                                                        <h2 class="text-center text-xl font-bold">RealPix</h2>
+                                                        <p className='my-3'>A camera management warehouse website</p>
+                                                        <div class="card-actions justify-end">
+                                                            <a href="https://camera-warehouse-9d161.web.app/"><button class="btn btn-primary">Visit</button></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+        </main>
     );
 };
 

925c5806777f1400e5bd3bbcb7a495fe4036b00a
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-26 18:27:38 +0600

    Added transaction id

--- src/Components/Dashboard/OrderedProduct.js
+++ src/Components/Dashboard/OrderedProduct.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
 
 const OrderedProduct = ({ order }) => {
 
-    const { _id, orderedProduct, orderedQuantity, productImage, productPrice, paid } = order;
+    const { _id, orderedProduct, orderedQuantity, productImage, productPrice, paid, customerName, customerEmail, customerAddress, transactionId } = order;
 
     const totalPrice = Number(orderedQuantity) * Number(productPrice);
 
@@ -32,10 +33,16 @@ const OrderedProduct = ({ order }) => {
             <th>
                 {
                     !paid ?
-                        <Link to={`/dashboard/payment/${_id}`}><button className="btn btn-success btn-xs">Pay $</button></Link>
+                        <Link to={`/dashboard/payment/${_id}`}><button className="btn btn-success w-full btn-sm">Pay $</button></Link>
                         :
-                        <button className='btn btn-ghost btn-xs'>PAID</button>
+                        <>
+                            <div className='flex flex-col'>
+                                <p className='mx-auto text-green-600 mb-2'>PAID<BsFillCheckCircleFill className='text-green-600 inline-block -mt-1 text-lg ml-2' /></p>
+                                <p className='text-xs'>Transaction ID : <span className='text-green-600'>{transactionId}</span></p>
+                            </div>
+                        </>
                 }
+
             </th>
         </tr>
 

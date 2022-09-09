b01a76fffb131b3672703e952393624f81f49e61
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-09-09 18:01:08 +0600

    About us completed

--- src/Components/Contact/ContactUs.js
+++ src/Components/Contact/ContactUs.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import PageTitle from '../PageTitle/PageTitle';
+import contact from '../../Assets/contactus.png'
 
 const ContactUs = () => {
-    return (
-        <div>
 
-        </div>
+    const formControl = e => {
+        e.preventDefault();
+        e.target.reset();
+        console.log('click')
+    }
+
+    return (
+        <section className='lg:px-24 px-3 my-28'>
+
+            <PageTitle title="Contact Us" />
+
+            <div className='bg-white p-7 rounded-lg shadow-lg'>
+                <h1 className='lg:text-5xl text-3xl text-center lg:leading-relaxed font-semibold capitalize'>How can we help?</h1>
+                <p className='text-center lg:w-2/5 mx-auto my-5'>While we’re good with smoke signals, there are simpler ways for us to get in touch and answer your questions.</p>
+
+                <div className='flex justify-center mt-7'>
+                    <button className='btn text-white capitalize mx-3'>Frequently Asked Questions</button>
+                    <button className='btn text-white capitalize mx-3'>Privacy Policy</button>
+                </div>
+
+            </div>
+
+            <div className='lg:w-[1196px] sm:block lg:flex justify-around items-center mx-auto text-justify mt-24 lg:mt-0 lg:h-screen'>
+
+                <img src={contact} className="lg:w-[45%] drop-shadow-xl mb-10 lg:mb-0" alt="" />
+
+                <div className=' sm:w-full lg:w-[60%] sm:ml-0 mt-5 lg:mt-0 lg:ml-10'>
+                    <form onSubmit={formControl}>
+                        <div className='flex justify-between items-center'>
+                            <input type="text" name='fname' placeholder="First Name" class="input input-bordered text-[#0B1B28] p-4 bg-[#FFF9F9] w-full mr-2" />
+                            <input type="text" name='lname' placeholder="Last Name" class="input input-bordered text-[#0B1B28] p-4  bg-[#FFF9F9] w-full ml-2" />
+                        </div>
+                        <br />
+                        <input type="email" name='email' placeholder="Email" class="input input-bordered text-[#0B1B28] p-4 bg-[#FFF9F9] w-full" />
+                        <br />
+                        <input type="text" name='msg' placeholder="Message" class="textarea mt-5 h-44 textarea-bordered text-[#0B1B28] p-4 bg-[#FFF9F9] w-full" />
+                        <input type="submit" className='btn w-full text-white mt-5' value="Submit" />
+                    </form>
+                </div>
+
+            </div>
+
+        </section>
     );
 };
 

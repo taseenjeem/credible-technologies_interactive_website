811d8ef5b0eff5a7f2dcca3838a30d1354d7f519
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-09-09 12:44:38 +0600

    Minor changes

--- src/Components/Home/Review.js
+++ src/Components/Home/Review.js
@@ -12,8 +12,10 @@ const Review = ({ review }) => {
             </div>
             <div>
                 <h2 className="text-gray-800 text-3xl font-semibold">{name}</h2>
-                <h2 className="text-gray-800 text-xl font-semibold">{rate} out of 5<BsStarFill className='inline-block -mt-2 ml-2 text-yellow-500' /></h2>
-                <p className="mt-2 text-gray-600">{message}</p>
+                <h2 className="text-gray-800 text-lg font-semibold my-2">{rate} out of 5<BsStarFill className='inline-block -mt-2 ml-2 text-yellow-500' /></h2>
+                <div className='overflow-y-auto h-[200px]'>
+                    <p className="mt-2 text-gray-600">{message}</p>
+                </div>
             </div>
             <div className="flex justify-end mt-4">
                 <a href="/" className="text-md text-gray-400 font-medium ">{location}</a>

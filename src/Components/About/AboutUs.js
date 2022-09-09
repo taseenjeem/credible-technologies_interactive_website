2753cfafcefe9d2aea1dd9636098a95ee1c022b5
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-09-09 15:10:25 +0600

    About page half done

--- src/Components/About/AboutUs.js
+++ src/Components/About/AboutUs.js
@@ -2,9 +2,60 @@ import React from 'react';
 
 const AboutUs = () => {
     return (
-        <div>
+        <section className='lg:px-24 px-3 my-28'>
 
-        </div>
+            <div className='bg-white p-7 rounded-lg shadow-lg'>
+                <h1 className='lg:text-5xl text-3xl text-center lg:leading-relaxed font-semibold capitalize'>The Game Is Being <br />Changed by Us</h1>
+                <p className='text-center lg:w-2/5 mx-auto my-5'>A significant part of people's lives now is the network, as a result of raising people's standards of living.</p>
+
+                <div className='flex justify-center mt-7'>
+                    <button className='btn text-white capitalize mx-3'>Get Started</button>
+                    <button className='btn text-white capitalize mx-3'>Privacy Policy</button>
+                </div>
+
+                <div className='absolute top-[30%] left-[80%] h-[15px] w-[15px] rounded-full bg-[#452473]'></div>
+                <div className='absolute top-[40%] left-[20%]  h-[20px] w-[20px] rounded-full bg-[#e3614a]'></div>
+                <div className='absolute top-[60%] left-[70%]  h-[20px] w-[20px] rounded-full bg-[#16162E]'></div>
+                <div className='absolute top-[70%] right-[20%]  h-[10px] w-[10px] rounded-full bg-[#46c08f]'></div>
+            </div>
+
+            <div className='container text-left sm:block lg:flex justify-around items-center mt-[100px] w-10/12 mx-auto '>
+                <div className='w-full lg:w-[40%] relative mb-16 lg:mb-0'>
+                    <div className='mb-12'>
+                        <h4 className='text-[#65C3C8] text-3xl font-bold mb-4'>Our story</h4>
+                        <p className='text-[#0B1B28] text-[18px] font-semibold'>We are building the economic infrastructure for the internet. Businneses of every size.</p>
+                    </div>
+                    <div className='flex justify-center items-center '>
+                        <div className='mt-4'>
+
+                            <div className='relative h-[70px] w-[70px] bg-[#16162E] rounded-full'>
+                                <div className='absolute h-[70px] w-[70px] top-[-55%] bg-base-100 '></div>
+                                <div className='absolute top-[0%] left-[0%] h-[30px]  overflow-hidden w-[30px] bg-[#452473] rounded-full'></div>
+                                <div className='absolute top-[0%] left-[50%] h-[30px] overflow-hidden w-[30px] bg-[#FF745C] rounded-full'></div>
+                            </div>
+                        </div>
+                        <div className=''>
+                            <p className='text-[#0B1B28] text-[14px] mt-8 pl-4 sm:pr-0 lg:pr-8'>A living place for curiosity and collaboration meeting and meaning. <br />The heart of a creative community.</p>
+                        </div>
+
+                    </div>
+                    <div className='absolute top-[50%] left-[40%] h-[10px] w-[10px] rounded-full bg-[#452473]'></div>
+                    <div className='absolute top-[50%] right-[0%] h-[15px] w-[15px] rounded-full bg-[#FF745C]'></div>
+                </div>
+
+                <div className='sm:w-full lg:w-[50%] lg:ml-52 pt-4'>
+                    <div>
+                        <h4 className='text-[18px] text-[#0B1B28] font-bold mb-2'>01. 6 years of intence research </h4>
+                        <p className='text-[14px] text-[#0B1B28] ml-6 mb-5'>Our founders had a solution. This is your Email marketing specilist, newsslatter service, and campaign manager, all rolled into one.</p>
+                        <h4 className='text-[18px] text-[#0B1B28] font-bold mb-2'>02. Audience segmentation</h4>
+                        <p className='text-[14px] text-[#0B1B28] ml-6 mb-5'>Our founders had a solution. This is your Email marketing specilist, newsslatter service, and campaign manager, all rolled into one.</p>
+                        <h4 className='text-[18px] text-[#0B1B28] font-bold mb-2'>03. Contact monitoring</h4>
+                        <p className='text-[14px] text-[#0B1B28] ml-6'>Our founders had a solution. This is your Email marketing specilist, newsslatter service, and campaign manager, all rolled into one.</p>
+                    </div>
+                </div>
+            </div>
+
+        </section>
     );
 };
 

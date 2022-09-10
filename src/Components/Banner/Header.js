05f015bbf6436e2cd4dd26c324c21738edce4814
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-09-10 12:43:21 +0600

    bg changed

--- src/Components/Banner/Header.js
+++ src/Components/Banner/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
                 <div className='text-white'>
                     <h1 className='text-2xl font-bold underline underline-offset-4'>Welcome to</h1>
                     <h1 className="my-5 text-5xl lg:text-7xl font-bold" style={{ fontFamily: "Ubuntu" }}><img className='App-logo lg:w-32 w-16 inline-block mt-[-10px]' src={logo} alt="" /> Credible Technologies</h1>
-                    <div className='lg:w-3/4 mx-auto mb-5 lg:leading-loose'>
+                    <div className='lg:w-3/4 mx-auto mb-5 lg:font-semibold lg:text-lg lg:leading-loose'>
                         <p>If you are looking for the best computer components manufacturer in the world you must consider Credible Technologies, as it is a leading computer parts and manufacturer company in the world. Selling computer Accessories and IT products all over the world through its website and worldwide shipping platform. It ensures a fastest , secure, and convenient online business experience with a wide range of product offering in many categories.</p>
                     </div>
                     <Link to="/about-us" className="btn btn-primary capitalize">Learn more about us</Link>

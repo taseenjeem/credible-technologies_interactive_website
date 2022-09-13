c854083407ed07cc5ec9af1622ed6f0d2840d68a
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-09-13 11:43:58 +0600

    Added link

--- src/Components/Home/Community.js
+++ src/Components/Home/Community.js
@@ -15,34 +15,34 @@ const Community = () => {
                 <h2 className="normal-case lg:text-2xl text-xl text-center font-bold lg:mb-20 mb-10" style={{ fontFamily: "Ubuntu" }}><img className='w-9 mb-1 mr-2 App-logo inline-block' src={logo} alt="" />Credible Technologies Communities</h2>
 
                 <div className='grid lg:grid-cols-4 gap-7'>
-                    <div className='flex justify-center items-center bg-white shadow-lg p-5 rounded-md'>
+                    <a href="https://www.facebook.com/taseenfuadjeem" target="_blank" rel='noreferrer' className='flex justify-center items-center bg-white shadow-lg p-5 rounded-md'>
                         <img className='w-24' src={facebook} alt="" />
                         <div className='ml-3'>
                             <h2 className='text-xl font-semibold'>Facebook</h2>
                             <p className='mt-2 text-sm'>Like our page for product reviews.</p>
                         </div>
-                    </div>
-                    <div className='flex justify-center items-center bg-white shadow-lg p-5 rounded-md'>
+                    </a>
+                    <a href="https://twitter.com/taseenfuadjeem" target="_blank" rel='noreferrer' className='flex justify-center items-center bg-white shadow-lg p-5 rounded-md'>
                         <img className='w-24' src={twitter} alt="" />
                         <div className='ml-3'>
                             <h2 className='text-xl font-semibold'>Twitter</h2>
                             <p className='mt-2 text-sm'>Follow for latest news and updates.</p>
                         </div>
-                    </div>
-                    <div className='flex justify-center items-center bg-white shadow-lg p-5 rounded-md'>
+                    </a>
+                    <a href="https://discord.gg/vCUdY5wwwK" target="_blank" rel='noreferrer' className='flex justify-center items-center bg-white shadow-lg p-5 rounded-md'>
                         <img className='w-24' src={discord} alt="" />
                         <div className='ml-3'>
                             <h2 className='text-xl font-semibold'>Discord</h2>
                             <p className='mt-2 text-sm'>Join our largest discord server.</p>
                         </div>
-                    </div>
-                    <div className='flex justify-center items-center bg-white shadow-lg p-5 rounded-md'>
+                    </a>
+                    <a href="https://t.me/SanctumSactorum" target="_blank" rel='noreferrer' className='flex justify-center items-center bg-white shadow-lg p-5 rounded-md'>
                         <img className='w-24' src={telegram} alt="" />
                         <div className='ml-3'>
                             <h2 className='text-xl font-semibold'>Telegram</h2>
                             <p className='mt-2 text-sm'>Join our largest telegram channel.</p>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
 
